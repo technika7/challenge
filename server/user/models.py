3e8eb0a1c3a8a534d37aac85ca133efaf0a76394
technika7 <technikavalley@gmail.com>
2024-08-12 09:23:12 +0530

    fix: Added Eth Wallet Address

--- server/user/models.py
+++ server/user/models.py
@@ -43,6 +43,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(null=False, blank=False, unique=True)
     first_name = models.CharField(max_length=50, blank=False, null=False, default='Default first name')
     last_name = models.CharField(max_length=50, blank=False, null=False, default='Default last name')
+    eth_wallet_address = models.CharField(max_length=50, blank=False, null=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
